37588ad273cb67858ede878b6cf5d5edb0166cdb
Wing leung CHOI <wingleungchoi@gmail.com>
2019-03-03 23:38:39 +0000

    able to take users input

--- week2/caesar.c
+++ week2/caesar.c
@@ -1,42 +1,50 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-void append(char* s, char c)
-{
-        int len = strlen(s);
-        s[len] = c;
-        s[len+1] = '\0';
-}
+int getLetterKey(char c);
+int shift(char c, int key);
 
-char* encrypt(char* plaintext);
-// int k = 1;
 int main(int argc, string argv[])
 {
-    printf("Argc: %2d\n", atoi(argv[1]));
-    int k = atoi(argv[1]);
+    if (argc < 2) {
+        printf ("Dont' forget to pass arguments!\n");
+        return(-1);
+    }
+    if (argc > 2) {
+        printf ("Usage: ./vigenere keyword\n");
+        return(-1);
+    }
     char* plaintext = get_string("Plaintext: ");
     printf("Plaintext input: %s\n", plaintext);
 
-    int n = strlen(plaintext);
-    char ciphertext[n];
+      for (int i = 0, n = strlen(plaintext); i < n; i++)
+      {
+          if (!isalpha(plaintext[i]))
+          {
+              printf("Usage: ./vigenere keyword\n");
 
-    for (int i = 0; i < n; i++)
-    {
-        char pi = plaintext[i];
-        for (int j = 0; j < k; j ++)
-        {
-            pi++;
-            ciphertext[i] = pi;
-        }
-        printf("ciphertext: %c\n", pi);
-    }
-    ciphertext[n] = '\0';
-    printf("Ciphertext: %s\n", ciphertext);
-    return 1;
+              return -1;
+          }
+         int letterKey = getLetterKey(argv[1][0]);
+         int newChar = shift(plaintext[i], letterKey);
+         printf("newChar %c\n", newChar);
+      }
 }
 
-char* encrypt(char* plaintext)
+int getLetterKey(char c)
 {
-    return plaintext;
+    c = tolower(c);
+    return (c - 'a') % 3;
+}
+
+int shift(char c, int key)
+{
+    // 'A' + (p[i] - 'A' + letterKey) % 26);
+    printf("c input %c\n", c);
+    int lowerCase = c - 'a';
+    int higherCase = c - 'A';
+    if (lowerCase >= 0)  return ('a' + (lowerCase + key));
+    return ('A' + (higherCase + key));
 }
