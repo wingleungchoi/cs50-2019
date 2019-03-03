8a47111fac7484e0071e26b3f57fde7a33a330da
Wing leung CHOI <wingleungchoi@gmail.com>
2019-03-03 20:03:49 +0000

    check if argument is entered

---
+++ week2/vigenere.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc < 2) {
+        printf ("Dont' forget to pass arguments!\n");
+        return(-1);
+    }
+    printf("Argc: %s\n", argv[1]);
+   
+}
