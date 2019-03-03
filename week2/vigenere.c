73ded19365ba9c6e397864918029e1f2888dca58
Wing leung CHOI <wingleungchoi@gmail.com>
2019-03-03 20:37:16 +0000

    able to check input

--- week2/vigenere.c
+++ week2/vigenere.c
@@ -1,5 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
@@ -7,6 +9,19 @@ int main(int argc, string argv[])
         printf ("Dont' forget to pass arguments!\n");
         return(-1);
     }
+    if (argc > 2) {
+        printf ("Usage: ./vigenere keyword\n");
+        return(-1);
+    }
     printf("Argc: %s\n", argv[1]);
-   
+
+  for (int i = 0, n = strlen(argv[1]); i < n; i++)
+  {
+      if (!isalpha(argv[1][i]))
+      {
+          printf("Usage: ./vigenere keyword\n");          
+          return -1;
+      }
+  }
+
 }
