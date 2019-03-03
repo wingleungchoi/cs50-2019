b1fa74abf09638a09fca8fc34ff159ce4ac9606b
Wing leung CHOI <wingleungchoi@gmail.com>
2019-03-03 21:58:48 +0000

    able to shift

--- week2/vigenere.c
+++ week2/vigenere.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <ctype.h>
 
+int shift(char c);
+
 int main(int argc, string argv[])
 {
     if (argc < 2) {
@@ -15,13 +17,28 @@ int main(int argc, string argv[])
     }
     printf("Argc: %s\n", argv[1]);
 
-  for (int i = 0, n = strlen(argv[1]); i < n; i++)
-  {
-      if (!isalpha(argv[1][i]))
+      for (int i = 0, n = strlen(argv[1]); i < n; i++)
       {
-          printf("Usage: ./vigenere keyword\n");          
-          return -1;
-      }
-  }
+          if (!isalpha(argv[1][i]))
+          {
+              printf("Usage: ./vigenere keyword\n");
 
+              return -1;
+          }
+         int key = shift(argv[1][i]);
+         printf("key %i\n", key);
+      }
+}
+
+int shift(char c)
+{
+    // 'A' + (p[i] - 'A' + letterKey) % 26);
+    printf("c input %c\n", c);
+    int key;
+    int lowerCase = (c - 'a') % 26;
+    int higherCase = (c - 'A') % 26;
+//     if (lowerCase > 0)  return lowerCase;
+    printf("lowerCase: %d\n", lowerCase);
+    printf("higherCase: %d\n", higherCase);
+    return 0;
 }
